637c9f22b6110781fbb50b40240e54ddb799f5bc
ziaur-rahman44 <ziaurfahim44@gmail.com>
2021-02-03 02:30:25 +0600

    true and false value

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// Falsfy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy:
+// " ", "0" and others
+
+let number = 0;
+if(number){
+    console.log("This is true");
+}
+else{
+    console.log("This is false");
+}
