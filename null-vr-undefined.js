1f4b5680a33a312e96de79328ea6c23a5ab08886
ziaur-rahman44 <ziaurfahim44@gmail.com>
2021-02-03 02:56:32 +0600

    null vr undefined clear for interview

---
+++ null-vr-undefined.js
@@ -0,0 +1,17 @@
+// Undefined:
+let number
+function some(num){
+    console.log(num);
+    return
+}
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const numbers = add(11);
+console.log(numbers);
+const person = {name:"Sahin", phone:000000};
+console.log(person.gh);
+
+// null:
+const number = null;
+console.log(number);
